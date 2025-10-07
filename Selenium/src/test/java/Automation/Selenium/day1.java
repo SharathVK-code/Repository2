fda6924e3aa62235e9f6abed987bc7e2ece319d3
ambat <ambat@SharathVirat>
2025-10-07 10:30:46 +0530

    07

--- Selenium/src/test/java/Automation/Selenium/day1.java
+++ Selenium/src/test/java/Automation/Selenium/day1.java
@@ -8,6 +8,7 @@ public class day1 {
 	public static void main(String[] args) {
 
 		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
 		driver.get("https://www.google.com");
 
 	}
