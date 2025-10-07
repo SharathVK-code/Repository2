00e9011fa23a4b3673243c92912ed9206c335437
ambat <ambat@SharathVirat>
2025-10-07 10:27:15 +0530

    stabilized POM.XML changed

---
+++ Selenium/src/test/java/Automation/Selenium/day1.java
@@ -0,0 +1,15 @@
+package Automation.Selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class day1 {
+
+	public static void main(String[] args) {
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+
+	}
+
+}
