f0280ef8fe60a7e6b3cd31790fedcc2c73105e70
ambat <ambat@SharathVirat>
2025-10-08 00:07:54 +0530

    up to check out

--- Selenium/src/test/java/Automation/Selenium/zaraCoatToCart.java
+++ Selenium/src/test/java/Automation/Selenium/zaraCoatToCart.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class day1 {
+public class zaraCoatToCart {
 
 	public static void main(String[] args) {
 
