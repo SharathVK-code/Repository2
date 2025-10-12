7bdcffda1a6e60dbd40530030ac16de8a812696a
ambat <ambat@SharathVirat>
2025-10-12 12:54:51 +0530

    commit

--- Selenium/src/test/java/Automation/Selenium/Base_E2E.java
+++ Selenium/src/test/java/Automation/Selenium/Base_E2E.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class zaraCoatToCart {
+public class Base_E2E {
 
 	public static void main(String[] args) throws InterruptedException {
 
