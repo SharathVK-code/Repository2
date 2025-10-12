30cb6661b4f1c93cc71365145404b0b539c3cffa
ambat <ambat@SharathVirat>
2025-10-12 12:59:50 +0530

    TheFirstCode

--- Selenium/src/test/java/Automation/Selenium/ABC_TheFirstCode.java
+++ Selenium/src/test/java/Automation/Selenium/ABC_TheFirstCode.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class TheFirstCode {
+public class ABC_TheFirstCode {
 
 	public static void main(String[] args) throws InterruptedException {
 
