d25adeb905466cff3f62cd78a26e7391240e0bdd
ambat <ambat@SharathVirat>
2025-10-15 22:55:50 +0530

    copy e2e

---
+++ Selenium/src/test/java/Automation/Tests/POM_Standard_e2e_Copy.java
@@ -0,0 +1,60 @@
+package Automation.Tests;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import Automation.PageObjects.CheckOutPage;
+import Automation.PageObjects.LandingPage;
+import Automation.PageObjects.PaymentPage;
+import Automation.PageObjects.ProductCatalog;
+import Automation.PageObjects.SuccessPage;
+
+public class POM_Standard_e2e_Copy {
+
+	@Test
+	public void submitOrder() throws IOException, InterruptedException {
+
+		String desiredProduct = "ZARA COAT 3";
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+		// Landing page
+		LandingPage landingPage = new LandingPage(driver);
+		landingPage.goTo();
+		landingPage.loginUser("ambati.sharath500@gmail.com", "SharathVK@18");
+
+		// Product catalog
+		ProductCatalog productCatalog = new ProductCatalog(driver);
+		List<WebElement> prod = productCatalog.GetProductsList();
+		productCatalog.addProductToCart(desiredProduct);
+
+		// CheckOut page
+		CheckOutPage checkOutPage = new CheckOutPage(driver);
+		boolean flag = checkOutPage.VerifyProducrtDisplay(desiredProduct);
+		Assert.assertTrue(flag);
+		Thread.sleep(2000);
+		checkOutPage.ClickOnCheckOut();
+
+		// payment page
+		PaymentPage paymentPage = new PaymentPage(driver);
+		paymentPage.EnterDetails(567, "Sharath", "India");
+
+		// success page
+		SuccessPage successPage = new SuccessPage(driver);
+		String cnfrmMessage = successPage.validateConfirmMessage();
+		Assert.assertEquals(cnfrmMessage, "THANKYOU FOR THE ORDER.");
+		successPage.printOrderID();
+
+		driver.close();
+
+	}
+
+}
