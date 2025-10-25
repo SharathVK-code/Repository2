2d46ff5b2b35a9ab809425f48d9831eac29d0bed
ambat <ambat@SharathVirat>
2025-10-25 19:00:21 +0530

    om sai ram

--- Selenium/src/test/java/Automation/data/DataReader.java
+++ Selenium/src/test/java/Automation/data/DataReader.java
@@ -1,8 +1,21 @@
 package Automation.data;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import org.apache.commons.io.FileUtils;
+
 public class DataReader {
 
-	public void getJsonDataToMap() {
+	public void getJsonDataToMap() throws IOException {
+
+		// Read JSON to String
+		String jsonContent = FileUtils.readFileToString(
+				new File(System.getProperty("user.dir") + "\\src\\test\\java\\Automation\\data\\PurchaseOrder.json"),
+				StandardCharsets.UTF_8);
+
+		// convert string to HashMap - Jackson DataBind dependency
 
 	}
 
