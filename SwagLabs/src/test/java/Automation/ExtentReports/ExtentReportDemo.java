7377b08620e0e48faaed09dfed76fcccfec6a09e
ambat <ambat@SharathVirat>
2025-10-27 23:34:39 +0530

    om sai ram

--- SwagLabs/src/test/java/Automation/ExtentReports/ExtentReportDemo.java
+++ SwagLabs/src/test/java/Automation/ExtentReports/ExtentReportDemo.java
@@ -5,11 +5,22 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
 public class ExtentReportDemo {
 
 	@BeforeTest
 	public void config() {
 		// ExtentReports, ExtentSparkReporter
+		String path = System.getProperty("user.dir")+"\\reports\\index.html";
+		ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+		reporter.config().setReportName("Report_reportName");
+		reporter.config().setDocumentTitle("Report_Doc_Title");
+		
+		ExtentReports extent = new ExtentReports();
+		extent.attachReporter(reporter);
+		extent.setSystemInfo("Tester", "Sharath");
 		
 		
 	}
