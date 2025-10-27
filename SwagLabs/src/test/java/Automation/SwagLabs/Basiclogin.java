4a627421542fdbaba636c1aaaa5071246d814a40
ambat <ambat@SharathVirat>
2025-10-27 22:45:12 +0530

    test

--- SwagLabs/src/test/java/Automation/SwagLabs/Basiclogin.java
+++ SwagLabs/src/test/java/Automation/SwagLabs/Basiclogin.java
@@ -8,20 +8,19 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Basiclogin {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.saucedemo.com/");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.manage().window().maximize();
-		
-		
+
 		driver.findElement(By.id("user-name")).sendKeys("standard_user");
 		driver.findElement(By.id("password")).sendKeys("secret_sauce");
 		driver.findElement(By.id("login-button")).click();
-		
-		
-		
+		Thread.sleep(3000);
+		driver.switchTo().alert().accept();
+
 	}
 
 }
