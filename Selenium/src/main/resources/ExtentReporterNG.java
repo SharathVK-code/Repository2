e70feb857e9a5874896b78d93fbb4fd23bbca246
ambat <ambat@SharathVirat>
2025-10-30 10:38:02 +0530

    om namah shivaaya

---
+++ Selenium/src/main/resources/ExtentReporterNG.java
@@ -0,0 +1,21 @@
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentReporterNG {
+	
+	public void getReportObject() {
+		
+		// ExtentReports, ExtentSparkReporter
+				String path = System.getProperty("user.dir") + "\\reports\\index.html";
+				ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+				reporter.config().setReportName("Report_reportName");
+				reporter.config().setDocumentTitle("Report_Doc_Title");
+
+				extent = new ExtentReports();
+				extent.attachReporter(reporter);
+				extent.setSystemInfo("Tester", "Sharath");
+				extent.setSystemInfo("Org", "TCS");
+		
+	}
+
+}
