3e9717208678ba055facb1a21f3599bdb63ef935
ambat <ambat@SharathVirat>
2025-11-06 22:32:34 +0530

    test

--- Selenium/src/test/java/Automation/TestComponents/Test_Listeners.java
+++ Selenium/src/test/java/Automation/TestComponents/Test_Listeners.java
@@ -4,20 +4,30 @@ import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+
 public class Test_Listeners implements ITestListener {
+
+	ExtentTest test;
+	ExtentReports extent = ExtentReporterNG.getReportObject();
 
 	@Override
 	public void onTestStart(ITestResult result) {
+		test = extent.createTest(result.getMethod().getMethodName());
 
 	}
 
 	@Override
 	public void onTestSuccess(ITestResult result) {
+		test.log(Status.PASS, "Test passed");
 
 	}
 
 	@Override
 	public void onTestFailure(ITestResult result) {
+		test.fail(result.getThrowable());
 
 	}
 
