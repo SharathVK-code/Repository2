b0493f56ab437af2e38194546264e2beb4556e66
ambat <ambat@SharathVirat>
2025-11-18 22:18:08 +0530

    test

--- Selenium/src/test/java/Automation/TestComponents/BaseTest.java
+++ Selenium/src/test/java/Automation/TestComponents/BaseTest.java
@@ -14,6 +14,7 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
@@ -44,7 +45,9 @@ public class BaseTest {
 //		 password = SharathVK@18
 
 		if (browserName.equalsIgnoreCase("chrome")) {
-			driver = new ChromeDriver();
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("headless");
+			driver = new ChromeDriver(options);
 
 		} else if (browserName.equalsIgnoreCase("FireFox")) {
 			driver = new FirefoxDriver();
